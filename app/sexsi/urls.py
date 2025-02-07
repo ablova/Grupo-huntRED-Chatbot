ea30e899b90271c40c33f0ea22069a533ed8c86a
Pablo Lelo de Larrea H <pablollh@MacBook-Air-PLLH.local>
2025-02-06 18:38:23 -0600

    Mejoras en SEXSI, OTP, Firma Autografa, etc ...

--- app/sexsi/urls.py
+++ app/sexsi/urls.py
@@ -3,7 +3,7 @@
 from django.urls import path
 from app.sexsi.views import (
     create_agreement, agreement_detail, sign_agreement, 
-    download_pdf, save_biometric_signature, save_signature
+    download_pdf, upload_signature_and_selfie
 )
 
 app_name = 'sexsi'
@@ -13,6 +13,5 @@ urlpatterns = [
     path('agreement/<int:agreement_id>/', agreement_detail, name='agreement_detail'),
     path('sign/<int:agreement_id>/<str:signer>/<uuid:token>/', sign_agreement, name='sign_agreement'),
     path('download/<int:agreement_id>/', download_pdf, name='download_pdf'),
-    path('sign/biometric/<int:agreement_id>/<str:signer>/<uuid:token>/', save_biometric_signature, name='save_biometric_signature'),
-    path('sign/save/<int:agreement_id>/<str:signer>/<uuid:token>/', save_signature, name='save_signature'),
+    path('sign/save/<int:agreement_id>/<str:signer>/<uuid:token>/', upload_signature_and_selfie, name='save_signature'),
 ]
