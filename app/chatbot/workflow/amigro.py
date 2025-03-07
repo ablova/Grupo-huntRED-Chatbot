5c8adf02712cfd4170978f498c2347e2784fdb67
Pablo Lelo de Larrea H <pablollh@MacBook-Air-PLLH.local>
2025-03-07 13:22:01 -0600

    Integracion de funcionalidad salario, logging, slack, mejoras en nlp, gpt, etc ...

--- app/chatbot/workflow/amigro.py
+++ app/chatbot/workflow/amigro.py
@@ -7,7 +7,7 @@ from celery import shared_task
 from app.models import Person, Vacante, BusinessUnit, Application
 from app.utilidades.signature.pdf_generator import generate_contract_pdf
 from app.utilidades.signature.digital_sign import request_digital_signature
-from app.chatbot.integrations.services import send_email, send_message, send_options, send_menu, MENU_OPTIONS_BY_BU, Services
+from app.chatbot.integrations.services import send_email, send_message, send_options, send_menu, MENU_OPTIONS_BY_BU
 
 logger = logging.getLogger(__name__)
 
