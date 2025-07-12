893d1bc497646193d6642c70601569e5ed358ba7
Pablo Lelo de Larrea <ablova@gmail.com>
2025-07-11 21:16:41 -0600

    fix: Remover importación de AURAAPIView que no existe

--- app/views/aura/__init__.py
+++ app/views/aura/__init__.py
@@ -4,9 +4,7 @@ Vistas para el sistema AURA de IA ética y responsable.
 """
 
 from .dashboard import AURADashboardView
-from .api import AURAAPIView
 
 __all__ = [
-    'AURADashboardView',
-    'AURAAPIView'
+    'AURADashboardView'
 ]
