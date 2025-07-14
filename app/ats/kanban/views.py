4798766ead9cf200058ef93a7ce7ea7deb8167bc
Pablo Lelo de Larrea <ablova@gmail.com>
2025-07-14 13:31:53 -0600

    Remove unused rbac imports from kanban views - functions not actually used

--- app/ats/kanban/views.py
+++ app/ats/kanban/views.py
@@ -1,5 +1,5 @@
-"""
-Vistas para el sistema Kanban de gestión de candidatos.
+# app/ats/kanban/views.py
+"""Vistas para el sistema Kanban de gestión de candidatos.
 Proporciona las interfaces y APIs necesarias para la manipulación 
 del tablero y las tarjetas Kanban.
 """
@@ -33,7 +33,6 @@ from app.ats.kanban.models import (
     KanbanComment, KanbanAttachment, KanbanNotification
 )
 from app.ats.kanban.ml_integration import KanbanMLIntegration, get_ml_recommendations
-# from app.ats.utils.rbac import check_permission, has_organization_access
 from app.ats.utils.cache import cache_result
 from app.ats.utils.logger_utils import get_module_logger
 from app.ats.integrations.services import send_message
